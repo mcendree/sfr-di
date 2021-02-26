17920ae16efaa0be61a357553f40a702a271bf47
Trebuche <mcendree_adam@yahoo.com>
2021-02-26 17:56:41 -0600

    init add2

---
+++ src/test/java/guru/springframework/sfrdi/SfrDiApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfrdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfrDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
